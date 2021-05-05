8613ed34aa044c23f842f5e22e6c320ac0052576
Nick Young <nyoung458@gmail.com>
2021-05-05 16:12:53 -0500

    Movement and Camera
    Added player model, player movement, and third person camera

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    [Header("Player Attribuites")]
+    public float _jumpHeight = 3f;
+    public float _gravity = -9.81f;
+    public float _speed = 1f;
+    public float _turnSmoothTime = 0f;
+    float _turnSmoothVelocity;
+
+    [Header("Ground Check")]
+    public float groundDistance = 0.4f;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x, 0f, z).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * _speed * Time.deltaTime);
+        }
+        
+        velocity.y += _gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (isGrounded == true)
+            {
+
+            }
+        }
+    }
+}
