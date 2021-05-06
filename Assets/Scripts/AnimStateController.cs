6100c1bc0dd5312e556e39f5a068838d34b96580
Nick Young <nyoung458@gmail.com>
2021-05-05 21:01:58 -0500

    Animation
    Added idle, moving, and crouched animations/states for the player.

---
+++ Assets/Scripts/AnimStateController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimStateController : MonoBehaviour
+{
+    Animator animator;
+    int isMovingHash;
+    [SerializeField] ThirdPersonMovement _playerMovement;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isMovingHash = Animator.StringToHash("isMoving");
+    }
+
+    private void Update()
+    {
+        bool isMoving = animator.GetBool(isMovingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool leftPressed = Input.GetKey("a");
+        bool rightPressed = Input.GetKey("d");
+        bool backPressed = Input.GetKey("s");
+
+        if (!isMoving && (forwardPressed || leftPressed || rightPressed || backPressed))
+        {
+            Debug.Log("I LIKE TO MOVE IT MOVE IT");
+            animator.SetBool(isMovingHash, true);
+        }
+        if (isMoving && !forwardPressed && !leftPressed && !rightPressed && !backPressed)
+        {
+            animator.SetBool(isMovingHash, false);
+            Debug.Log("RED LIGHT");
+        }
+
+
+        if (Input.GetKey("c"))
+        {
+            Debug.Log("GET DOWN MR PRESIDENT");
+            animator.SetBool("isCrouched", true);
+            _playerMovement.enabled = false;
+            
+        }
+        if (!Input.GetKey("c"))
+        {
+            Debug.Log("Whew. Close one");
+            animator.SetBool("isCrouched", false);
+            _playerMovement.enabled = true;
+        }
+    }
+}
